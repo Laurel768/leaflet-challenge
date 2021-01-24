a922ba3393bd61c3302d6a4361ba641ba3241467
Laurel768 <laurel.whitten@hotmail.com>
2021-01-24 11:35:03 -0500

    added legend

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,37 +1,73 @@
 // Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
-});
+// function to determine marker size based on magnitude
+function markerSize(magnitude) {
+  return magnitude * 5;
+}
 
+// function to return the color based on magnitude
+function markerColor(magnitude) {
+if (magnitude > 4) {
+  return 'red'
+} else if (magnitude > 3) {
+  return 'orange'
+} else if (magnitude > 2) {
+  return 'yellow'
+} else {
+  return 'green'
+}
+}
 
-
-
-
-
-
-
-function createFeatures(earthquakeData) {
-
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + 
-      "</h3><hr><p>" + ("Magnitude: " + feature.properties.mag) + "</p>");
+// function for opacity based on magnitude
+function markerOpacity(magnitude) {
+  if (magnitude > 6) {
+    return .99
+  } else if (magnitude > 5) {
+    return .80
+  } else if (magnitude > 4) {
+    return .70
+  } else if (magnitude > 3) {
+    return .60
+  } else if (magnitude > 2) {
+    return .50
+  } else if (magnitude > 1) {
+    return .40
+  } else {
+    return .30
   }
+}
 
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
+// GET request, and function to handle returned JSON data
+d3.json(queryUrl, function(data) {
+  
+  var earthquakes = L.geoJSON(data.features, {
+    onEachFeature : addPopup,
+    pointToLayer: addMarker
   });
 
-  // Sending our earthquakes layer to the createMap function
+// call function to create map
   createMap(earthquakes);
+
+});
+
+function addMarker(feature, location) {
+  var options = {
+    stroke: false,
+    fillOpacity: markerOpacity(feature.properties.mag),
+    color: markerColor(feature.properties.mag),
+    fillColor: markerColor(feature.properties.mag),
+    radius: markerSize(feature.properties.mag)
+  }
+
+  return L.circleMarker(location, options);
+
+}
+
+// Define a function we want to run once for each feature in the features array
+function addPopup(feature, layer) {
+  // Give each feature a popup describing the place and time of the earthquake
+  return layer.bindPopup(`<h3> ${feature.properties.place} </h3> <hr> <h4>Magnitude: ${feature.properties.mag} </h4> <p> ${Date(feature.properties.time)} </p>`);
 }
 
 function createMap(earthquakes) {
@@ -70,6 +106,27 @@ function createMap(earthquakes) {
   // Add the layer control to the map
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
-  }).addTo(myMap);
+  }).addTo(myMap);  
+
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (myMap) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 1, 2, 3, 4],
+        color = ['red', 'orange', 'yellow', 'green'];
+      
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + color[i] + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
 }
-     
--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -5,10 +5,6 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 var plateURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json"
 
 
-
-
-
-
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
@@ -24,11 +20,6 @@ function createFeatures(earthquakeData) {
       "</h3><hr><p>" + new Date(feature.properties.time) + 
       "</h3><hr><p>" + (feature.properties.mag) + "</p>");
   }
-
-
-
-
-
 
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
@@ -55,10 +46,6 @@ d3.json(plateURL, function (geoJson) {
 })
 
 
-
-
-
-
 function createMap(earthquakes) {
 
   // Define streetmap and darkmap layers
@@ -79,10 +66,12 @@ function createMap(earthquakes) {
   });
 
 
-  var outmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  var outmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
     maxZoom: 18,
-    id: "outdoors-v11",
+    zoomOffset: -1,
+    id: "mapbox/outdoors-v11",
     accessToken: API_KEY
   });
 
@@ -116,4 +105,5 @@ function createMap(earthquakes) {
     collapsed: false
   }).addTo(myMap);
 }
-     
+
+
